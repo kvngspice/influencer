5fff580056c98f3c7bb0a96303bcbf6305dfa105
kvngspice <sampelumibiz@gmail.com>
2025-02-23 13:13:00 +0100

    Your commit message3

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-chmod +x build.sh
+#!/bin/bash
+set -o errexit  # exit on error
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
